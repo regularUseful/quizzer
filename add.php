bf3141802f9d1e955f10308d9d37dfe4d901d004
Nathan Scattini <scattini3@hotmail.com>
2020-01-07 03:12:34 +1030

    added 'add question' functionality

--- add.php
+++ add.php
@@ -1,3 +1,58 @@
+<?php 
+    include "database.php";
+    if(isset($_POST['submit'])){
+        //question
+        $question_number = $_POST['question_number'];
+        $question_text = $_POST['question_text'];
+        //answer
+        $choices = array();
+        $choices[1] = $_POST['choice1'];
+        $choices[2] = $_POST['choice2'];
+        $choices[3] = $_POST['choice3'];
+        $choices[4] = $_POST['choice4'];
+        $choices[5] = $_POST['choice5'];
+        $correct_choice = $_POST['correct_choice'];
+
+        //set query
+        $query = "INSERT INTO `questions` (question_number, text) VALUES ('$question_number', '$question_text')";
+
+        //run query
+        $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+        if($insert_row){
+            foreach($choices as $choice => $value){
+                if($value != ""){
+                    if($correct_choice == $choice){
+                        $is_correct = 1;
+                    }
+                    else{
+                        $is_correct = 0;
+                    }
+                    $query = "INSERT INTO `choices` (question_number, is_correct, text) VALUE ('$question_number', '$is_correct', '$value')";
+                    
+                    //run query
+                    $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+                    //validate
+                    if($insert_row){
+                        continue;
+                    }
+                    else{
+                        die('ERROR : ' . $mysqli->errno . $mysqli->error);
+                    }
+                }
+            }
+            $msg = "Question added";
+        }
+    }
+    
+        //get total questions
+        $query = "SELECT * FROM `questions`";
+        $questions = $mysqli->query($query) or die($mysqli->error.__LINE__);
+        $total = $questions->num_rows;
+        $next = $total + 1;
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -14,10 +69,13 @@
         <main>
             <div class="container">
                <h2>Add A Question</h2>
-                <form action="add.php" method="POST"></form>
+               <p class="added">
+                   <?php if(isset($msg)){echo $msg; }?>
+               </p>
+                <form action="add.php" method="POST">
                 <p>
-                    <label for="question_number">Question NUmber: </label>
-                    <input type="number" name="question_number">
+                    <label for="question_number">Question Number: </label>
+                    <input type="number" name="question_number" value="<?php echo $next; ?>">
                </p>
                <p>
                     <label for="question_text">Question Text: </label>
@@ -50,6 +108,7 @@
                <p>
                     <input type="submit" name="submit" value="submit">
                </p>
+               </form>
             </div>
         </main>
         <footer>
